4040592fed8816b3137ea7b2007c3b5ad9ab764b
Amro Ghoneim <amroghoneim@aucegypt.edu>
2020-11-08 17:58:07 +0000

    added Docstrings to functions to Board class

--- triple_t/Board.py
+++ triple_t/Board.py
@@ -1,17 +1,26 @@
 class Board():
-    def __init__(self, length= 3, width = 3):
-        self.length = length
-        self.width = width 
+    def __init__(self, dim= 3):
+        '''
+            init function for Board class
+            
+            dim: int, length and width dimensions for board
+            game_board: list, Data structure to act as game board
+        '''
+        self.dim = dim
         self.game_board = []
         print("Created Board")
         
     def initialize_board(self):
-        self.game_board = [['-']*self.width for _ in range(self.length)]
+        '''
+            Function to initialize empty dim x dim board
+        '''
+        self.game_board = [['-']*self.dim for _ in range(self.dim)]
     
     def __repr__(self):
+
         out = ""
-        for i in range(self.length):
-            for j in range(self.width):
+        for i in range(self.dim):
+            for j in range(self.dim):
                 out = out + self.game_board[i][j]
             out = out + '\n'
         return out
