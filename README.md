49c09e6454327a4cac348aad2070647c72f6d446
Amro Ghoneim <amroghoneim@aucegypt.edu>
2020-11-08 17:39:59 +0000

    added installation and usage instructions to readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Project Description
 
 This project implements Tic-Tac-Toe package in python using an object oriented design.
+
+
+# Installation
+
+```
+pip install triple-t
+```
+
+# How to play
+
+```
+# import library
+from triple_t import Play
+
+game = Play()
+game.start_game()
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='triple-t',
+setup(name='triple_t',
       version='0.1',
       description='Tic Tac Toe game',
-      packages=['triple-t'],
+      packages=['triple_t'],
       author='Amro Ghoneim',
       author_email='amroghoneim@aucegypt.edu',
       zip_safe=False)
